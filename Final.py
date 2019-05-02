55e63f401f6860c52fce10f007a4d21303ba033a
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2019-05-02 06:24:54 -0600

    Added Comments, and Made debug statements
    Added Comments explaining everything, and Made debug statements to better inform the user

--- Final.py
+++ Final.py
@@ -1,8 +1,9 @@
 import requests
 auth_key= input("copy and paste your auth KEY:  ")
 room_title= input("Enter room name  ")
-
-email_list = input("Copy and paste your distro list here")
+####This takes the distro from a OUTLOOK TO FIELD and formats it so it can be injected later into a request
+### also a print distro for debug final filter
+email_list = input("Copy and paste your distro list here  ")
 email_list = email_list.replace('<',"")
 email_list = email_list.replace('>',"")
 email_list = email_list.replace(';',"")
@@ -14,15 +15,17 @@ for x in email_list:
         final_email_list.append(x)
 
 
-print(final_email_list)
-
+print("Here is your distro list {}".format(final_email_list))
+######
+####URls and headers for all requests made
 url_create_room = 'https://api.ciscospark.com/v1/rooms'
 url_add_users = 'https://api.ciscospark.com/v1/memberships'
 url_list_teams = 'https://api.ciscospark.com/v1/teams'
 
 headers = {'Authorization': 'Bearer {}'.format(auth_key),
            'Content-Type': 'application/json'}
-
+####
+### This block of code, requests users list of Team, and displays them to be choosen, to create a room in
 list_room = requests.api.get(url=url_list_teams,data=None,json='{}',headers=headers)
 list_room_filter = list_room.text
 list_room_filter = list_room_filter.replace('{','')
@@ -34,6 +37,7 @@ list_room_filter = list_room_filter.replace('"','')
 list_room_filter = list_room_filter.split(',')
 name_list = []
 id_list = []
+
 for x in list_room_filter:
     if 'name' in x:
         name_list.append(x.replace('name:',''))
@@ -45,11 +49,11 @@ options_rooms = dict(zip(name_list, id_list))
 
 for k in options_rooms:
     print(k)
-key_select = input("please type in TEAM name you wish to use leave blank for no team  ")
+key_select = input("please type in TEAM name you wish to use or leave blank for no team selection  ")
 final_key = options_rooms[key_select]
-
+#####
 data_createroom = {"title":"{}".format(room_title),"teamId":"{}".format(final_key)}
-
+##### Grabs newly created room ID number, to add users in a later function
 create_room = requests.api.post(url=url_create_room,data=None,json=data_createroom,headers=headers)
 
 room_id = create_room.text
@@ -59,9 +63,11 @@ room_id = room_id[0]
 room_id = room_id.split('"')
 room_id = room_id[3]
 room_id = str(room_id)
-print(room_id)
-
+print("This is your new room ID :  {}".format(room_id))
+####Debug for new room id
+##
+### This for loop runs through final_email_list to add users to just created room_id
 for x in final_email_list:
     data_adduser={"roomId":"{}".format(room_id),"personEmail":"{}".format(x)}
     add_users_to_room = requests.api.post(url=url_add_users,data=None,json=data_adduser,headers=headers)
-    print(add_users_to_room.text)
+    print("debug: adding user {} :: {}".format(x,add_users_to_room.text))
