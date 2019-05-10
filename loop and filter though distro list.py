53f0fe0fe73b7d1d9188c8f4ddbb4a8b08711973
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2019-05-10 07:00:50 -0600

    Add files via upload
    Alphabetized distro list, also included a username list.

--- loop and filter though distro list.py
+++ loop and filter though distro list.py
@@ -1,17 +1,38 @@
-distro = 'Penafiel, Jose <Jose.Penafiel@wwt.com>; Alsebei, Safwan <Safwan.Alsebei@wwt.com>; Brooks, Brittany <Brittany.Brooks@wwt.com>; Crist, Darius <Darius.Crist@wwt.com>; Dolinac, Zeljko <Zeljko.Dolinac@wwt.com>; Duran, Rosie <Rosie.Duran@wwt.com>; Erickson, Brian <Brian.Erickson@wwt.com>; Gibson, Geoffrey <Geoffrey.Gibson@wwt.com>; Harrington, Terri <Terri.Harrington@wwt.com>; Heber, Colton <Colton.Heber@wwt.com>; Hudson, Terra <Terra.Hudson@wwt.com>; LeBrun, Bryan <Bryan.LeBrun@wwt.com>; Medina, Julio <Julio.Medina@wwt.com>; Mulvanity, Ryon <Ryon.Mulvanity@wwt.com>; Narayan, Asheel <Asheel.Narayan@wwt.com>; Pass, Vic <Vic.Pass@wwt.com>; Penafiel, Jose <Jose.Penafiel@wwt.com>; Robinson, Joe <Joe.Robinson@wwt.com>; Smith, Francis <Francis.Smith@wwt.com>; Smith, Tamica <Tamica.Smith@wwt.com>; Strange, Chase <Chase.Strange@wwt.com>; Torres, Jonnie <Jonnie.Torres@wwt.com>; Valdez, Michelle <Michelle.Valdez@wwt.com>; Van Maanen, Eric <Eric.VanMaanen@wwt.com>; Warren, Justin <Justin.Warren@wwt.com>'
-distro = distro.replace('<',"")
+start_distro = input("please copy and paste distro from outlook   ")
+distro = start_distro.replace('<',"")
 distro = distro.replace('>',"")
 distro = distro.replace(';',"")
 distro = distro.split()
 
-
-final_distro = []
+name_distro = []
+email_distro = []
 for x in distro:
-
     if '@' in x:
-        final_distro.append(x)
+        email_distro.append(x)
 
 
+names = start_distro.replace('<',"")
+names = names.replace('>',"")
+names = names.replace(';',"")
+names = names.replace("@"," ")
+names = names.split()
 
-for x in final_distro:
+for x in names:
+    if '.' in x and ".com" in x:
+        pass
+    elif '.' in x:
+        name_distro.append(x)
+    else:
+        pass
+
+email_distro.sort(key=str.lower)
+cache = []
+
+
+for x in email_distro:
     print(x)
+
+
+
+
+
